943d8707fc94c3c5c1faa89adf8ebac08e46c909
Sam Atkins <butlers.tunic.04@icloud.com>
2023-11-13 20:19:15 +0000

    test: add fixtures

---
+++ tests/fixtures/main.py
@@ -0,0 +1,11 @@
+import json
+import os
+import re
+import sys
+from datetime import datetime
+from pathlib import Path
+
+import requests
+
+def main():
+    print("Hello World!")
---
+++ tests/fixtures/src/adapters/repo.py
@@ -0,0 +1,3 @@
+import alembic
+import sqlalchemy as sa
+from mako.template import Template
