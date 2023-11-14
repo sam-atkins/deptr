ecf07ffceb2c3526a88d21b0cc1080f68a7691a6
Sam Atkins <butlers.tunic.04@icloud.com>
2023-11-14 07:56:07 +0000

    Feature/track unused deps (#5)
    * refactor tests to use Result
    
    * feat: find unused deps
    
    * feat: add verbose flag

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,11 @@ use clap::Parser;
 use poetry::get_dependencies_from_pyproject;
 use python_ast::get_imports_from_src;
 use std::{
+    collections::HashMap,
     error::Error,
     path::{Path, PathBuf},
     string::String,
+    time::Instant,
 };
 
 type CliResult<T> = Result<T, Box<dyn Error>>;
@@ -28,6 +30,10 @@ struct Cli {
     /// Times the execution of the command (default: false)
     #[arg(short, long, default_value = "false")]
     timer: bool,
+
+    /// Verbose mode (default: false)
+    #[arg(short, long, default_value = "false")]
+    verbose: bool,
 }
 
 /// Config holds the configuration for the application
@@ -37,6 +43,7 @@ pub struct Config {
     toml_path: PathBuf,
     dev: bool,
     timer: bool,
+    verbose: bool,
 }
 
 /// get_args parses the command line arguments and returns a Config struct
@@ -45,6 +52,7 @@ pub fn get_args() -> CliResult<Config> {
     let path = cli.path.unwrap_or_else(|| ".".to_string());
     let dev = cli.dev;
     let timer = cli.timer;
+    let verbose = cli.verbose;
 
     let path_result = validators::valid_python_path(&path);
     let src_path: PathBuf;
@@ -64,20 +72,94 @@ pub fn get_args() -> CliResult<Config> {
         toml_path,
         dev,
         timer,
+        verbose,
     })
 }
 
 /// run executes the application
 pub fn run(config: Config) -> CliResult<()> {
+    let start = Instant::now();
+
     if config.dev {
-        println!("Tracking dev dependencies not currently supported. Proceeding with Production dependencies only.");
+        println!("");
+        println!("WARNING: Tracking dev dependencies not yet implemented. Proceeding with Production dependencies only.");
         println!("");
     }
 
     let manifest_deps = get_dependencies_from_pyproject(config.toml_path);
-    println!("{:?}", manifest_deps);
+    if config.verbose {
+        let log_statement = "Manifest dependencies: ";
+        print_keys(&manifest_deps, log_statement);
+    }
     let import_stmts = get_imports_from_src(&config.src_path);
-    println!("{:?}", import_stmts);
+    if config.verbose {
+        let log_statement = "Import statements: ";
+        print_keys(&import_stmts, log_statement);
+    }
+
+    let unused_deps = find_unused_manifest_deps(manifest_deps, import_stmts);
+    if unused_deps.len() == 0 {
+        println!("======================================");
+        println!("No unused dependencies found.");
+    } else {
+        println!("======================================");
+        println!("Possible unused manifest dependencies: ");
+        for dep in unused_deps.iter() {
+            println!("{}", dep);
+        }
+    }
+
+    if config.timer {
+        let duration = start.elapsed();
+        println!("======================================");
+        println!("Execution time: {:?}", duration);
+    }
 
     Ok(())
+}
+
+fn print_keys<V>(manifest_deps: &HashMap<String, V>, log_statement: &str) {
+    println!("======================================");
+    println!("{}", log_statement);
+    for (key, _value) in manifest_deps.iter() {
+        println!("{}", key);
+    }
+}
+
+fn find_unused_manifest_deps(
+    manifest_deps: HashMap<String, serde_json::Value>,
+    import_stmts: HashMap<String, bool>,
+) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for (key, _value) in manifest_deps.iter() {
+        if !import_stmts.contains_key(key) {
+            result.push(key.to_string());
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod lib {
+    use super::*;
+
+    #[test]
+    fn test_find_unused_manifest_deps() {
+        let mut manifest_deps = HashMap::new();
+        manifest_deps.insert(
+            "unused_dep".to_string(),
+            serde_json::Value::String("1.0.0".to_string()),
+        );
+        manifest_deps.insert(
+            "used_dep".to_string(),
+            serde_json::Value::String("1.0.0".to_string()),
+        );
+
+        let mut import_stmts = HashMap::new();
+        import_stmts.insert("used_dep".to_string(), true);
+
+        let unused_deps = find_unused_manifest_deps(manifest_deps, import_stmts);
+
+        assert_eq!(unused_deps, vec!["unused_dep"]);
+    }
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,27 +1,33 @@
 use assert_cmd::Command;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn runs_with_success() {
-    let mut cmd = Command::cargo_bin("deptr").unwrap();
-    cmd.arg("./tests/fixtures/");
-    let output = cmd.unwrap();
-    assert!(output.status.success());
+fn runs_with_success() -> TestResult {
+    Command::cargo_bin("deptr")?
+        .arg("./tests/fixtures/")
+        .assert()
+        .success();
+    Ok(())
 }
 
 #[test]
-fn runs_with_expected_error_no_pyproject_file() {
-    let mut cmd = Command::cargo_bin("deptr").unwrap();
-    cmd.assert().failure();
-    cmd.assert().code(1);
-    cmd.assert()
+fn runs_with_expected_error_no_pyproject_file() -> TestResult {
+    Command::cargo_bin("deptr")?
+        .assert()
+        .failure()
+        .code(1)
         .stderr("Error: Unable to find a pyproject.toml file\n");
+    Ok(())
 }
 
 #[test]
-fn runs_with_expected_error_invalid_path() {
-    let mut cmd = Command::cargo_bin("deptr").unwrap();
-    cmd.arg("bad/path");
-    cmd.assert().failure();
-    cmd.assert().code(1);
-    cmd.assert().stderr("Error: Invalid path provided\n");
+fn runs_with_expected_error_invalid_path() -> TestResult {
+    Command::cargo_bin("deptr")?
+        .arg("bad/path")
+        .assert()
+        .failure()
+        .code(1)
+        .stderr("Error: Invalid path provided\n");
+    Ok(())
 }
